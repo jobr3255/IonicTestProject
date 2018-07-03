d470985bf8ca9faceac560f1dca11c5599bdae46
Josh <jobr3255@colorado.edu>
2018-07-03 12:51:02 -0600

    Deleted old pages

--- src/providers/media-capture/media-capture.ts
+++ src/providers/media-capture/media-capture.ts
@@ -12,6 +12,35 @@ export class MediaCaptureProvider {
 
   constructor() {
     console.log('Hello MediaCaptureProvider Provider');
+
   }
+
+  startRecording(){
+    var options = { limit: 3 };
+    try{
+      Capture.navigator.device.capture.captureAudio(this.captureSuccess, this.captureError, options);
+    } catch (e){
+      console.log("An error occured:");
+      console.log(e.message);
+      console.log(e);
+    }
+  }
+
+  stopRecording(){
+
+  }
+
+  captureSuccess(mediaFiles) {
+    var i, path, len;
+    for (i = 0, len = mediaFiles.length; i < len; i += 1) {
+        path = mediaFiles[i].fullPath;
+        // do something interesting with the file
+      }
+  };
+
+  // capture error callback
+  captureError(error) {
+      Capture.navigator.notification.alert('Error code: ' + error.code, null, 'Capture Error');
+  };
 
 }
